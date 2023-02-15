974e06e1543114f2c2aaee4d056fb993219e2a52
Urszula Malinowska <ula.malin35@gmail.com>
2023-02-15 22:37:23 +0100

    seperate file with array specifics created

---
+++ src/specifics.js
@@ -0,0 +1,7 @@
+const specifics = [
+    { BMI: 18.5, BMIend:"", id: 1, meaning: "niedowaga" },
+    { BMI: 18.5 , BMIend: 24.99, id: 2, meaning: "wartość prawidłowa" },
+    { BMI: 25.0, BMIend:"", id: 3, meaning: "nadwaga" }
+  ]
+
+  export {specifics};

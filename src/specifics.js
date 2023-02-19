edec8b005e74cc4166f641ded54566eab30da72a
Urszula Malinowska <ula.malin35@gmail.com>
2023-02-19 02:07:22 +0100

    added to specifics array new object which indicates if person is obese

--- src/specifics.js
+++ src/specifics.js
@@ -1,12 +1,8 @@
 const specifics = [
   { BMIstart: 0, BMIend: 18.5, id: 1, meaning: "niedowaga" },
   { BMIstart: 18.5, BMIend: 25.0, id: 2, meaning: "wartość prawidłowa" },
-  { BMIstart: 25.0, BMIend: "",  id: 3, meaning: "nadwaga" }
+  { BMIstart: 25.0, BMIend: 29.9, id: 3, meaning: "nadwaga" },
+  { BMIstart: 30, BMIend: "", id: 4, meaning: "otyłość" },
 ]
-// const specifics = [
-//   { BMI: 0, id: 1, meaning: "niedowaga" },
-//   { BMI: 18.5,  id: 2, meaning: "wartość prawidłowa" },
-//   { BMI: 25.0, BMIend: "",  id: 3, meaning: "nadwaga" }
-// ]
 
 export { specifics };
